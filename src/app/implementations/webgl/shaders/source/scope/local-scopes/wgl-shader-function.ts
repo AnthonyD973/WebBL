b9bb250d968a5e2512ae7d85e460950d7f0a170d
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-01 17:17:25 -0400

    Partially implemented the function class.

--- src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-function.ts
+++ src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-function.ts
@@ -1,6 +1,7 @@
 import { WglShaderLocalScope } from '../wgl-shader-local-scope';
 import { WglShaderFunctionSignature } from '../../expression/types/wgl-shader-function-signature';
 import { ShaderLocalScope } from '../../../../../../api/shaders/source/scope/shader-local-scope';
+import { WglShaderConfig } from '../../../util/wgl-shader-config';
 
 export class WglShaderFunction extends WglShaderLocalScope {
 
@@ -13,6 +14,9 @@ export class WglShaderFunction extends WglShaderLocalScope {
 
     constructor(name: string, signature: WglShaderFunctionSignature) {
         super(null);
+        if (!WglShaderConfig.IDENTIFIER_REGEX.test(name)) {
+            throw new Error(`"${name}" is not a valid identifier`);
+        }
         this.name = name;
         this.signature = signature;
     }
