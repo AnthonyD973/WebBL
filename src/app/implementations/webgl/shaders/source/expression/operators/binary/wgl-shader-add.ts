75e3a5d6fea6b480e4e49b4a07c9987e848c55a3
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-04-30 21:04:15 -0400

    Implemented the add operator.

--- src/app/implementations/webgl/shaders/source/expression/operators/binary/wgl-shader-add.ts
+++ src/app/implementations/webgl/shaders/source/expression/operators/binary/wgl-shader-add.ts
@@ -1,6 +1,9 @@
 import { ShaderBinaryOperator } from '../../../../../../../api/shaders/source/expression/operators/shader-binary-operator';
 import { ShaderExpression } from '../../../../../../../api/shaders/source/expression/shader-expression';
 import { ShaderExpressionType } from '../../../../../../../api/shaders/source/expression/shader-expression-type';
+import { WglShaderAddVisitorDispatcher } from './visitor-dispatchers/wgl-shader-add-visitor-dispatcher';
+
+const OPERATOR = '+';
 
 export class WglShaderAdd implements ShaderBinaryOperator {
 
@@ -9,10 +12,19 @@ export class WglShaderAdd implements ShaderBinaryOperator {
     public readonly type: ShaderExpressionType;
 
     constructor(lhs: ShaderExpression, rhs: ShaderExpression) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+        this.type = this.lhs.type;
+        this.assertValid();
     }
 
     public parse(): string {
-        return null;
+        return this.lhs.parse() + ' ' + OPERATOR + ' ' + this.rhs.parse();
+    }
+
+    protected assertValid(): void {
+        const vd = new WglShaderAddVisitorDispatcher();
+        vd.visit(this.lhs.type, this.rhs.type);
     }
 
 }
