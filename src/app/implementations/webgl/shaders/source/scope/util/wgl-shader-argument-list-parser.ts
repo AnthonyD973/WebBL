cbd55b7f96e06d9a5f38ba835fd4739d151aa9b7
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-01 19:11:56 -0400

    Implemented the argument list parser.

--- src/app/implementations/webgl/shaders/source/scope/util/wgl-shader-argument-list-parser.ts
+++ src/app/implementations/webgl/shaders/source/scope/util/wgl-shader-argument-list-parser.ts
@@ -5,15 +5,45 @@ import { ShaderExpression } from '../../../../../../api/shaders/source/expressio
 export class WglShaderArgumentListParser {
 
     public parseSignature(params: ShaderExpressionType[]): string {
-        return null;
+        const parsedArgList = params.reduce((acc, param) => {
+            const isFirstParam = acc === '';
+            if (isFirstParam) {
+                return param.parse();
+            }
+            else {
+                return acc + ', ' + param.parse();
+            }
+        }, '');
+
+        return '(' + parsedArgList + ')';
     }
 
     public parseDeclaration(params: WglShaderVariable[]): string {
-        return null;
+        const parsedArgList = params.reduce((acc, param) => {
+            const isFirstParam = acc === '';
+            if (isFirstParam) {
+                return param.type.parse() + ' ' + param.name;
+            }
+            else {
+                return acc + ', ' + param.type.parse() + ' ' + param.name;
+            }
+        }, '');
+
+        return '(' + parsedArgList + ')';
     }
 
     public parseFunctionCall(params: ShaderExpression[]): string {
-        return null;
+        const parsedArgList = params.reduce((acc, param) => {
+            const isFirstParam = acc === '';
+            if (isFirstParam) {
+                return param.parse();
+            }
+            else {
+                return acc + ', ' + param.parse();
+            }
+        }, '');
+
+        return '(' + parsedArgList + ')';
     }
 
 }
