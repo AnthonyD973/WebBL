1b333162642360ee6dd8019d0c6a0072a077eeae
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-02 16:56:52 -0400

    Implemented the else if class.

--- src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-else-if.ts
+++ src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-else-if.ts
@@ -2,18 +2,20 @@ import { WglShaderIf } from './wgl-shader-if';
 import { WglShaderLocalScope } from '../wgl-shader-local-scope';
 import { ShaderExpression } from '../../../../../../api/shaders/source/expression/shader-expression';
 
+const TOKEN = 'else if';
+
 export class WglShaderElseIf extends WglShaderIf {
 
     public get scopeName(): string {
         return 'else if';
     }
 
-    constructor(condExpression: ShaderExpression) {
-        super(condExpression);
+    constructor(condition: ShaderExpression) {
+        super(condition);
     }
 
     public parse(): string {
-        return null;
+        return TOKEN + '(' + this.condition.parse() + ') ' + this.child.parse();
     }
 
 }
