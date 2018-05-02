21d36f04fcb663ac25f3952f197d870bf42481f8
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-02 10:48:49 -0400

    Made the child of the local scope part of the API, and made it default to an empty scope.

--- src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-empty-local-scope.ts
+++ src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-empty-local-scope.ts
@@ -1,17 +1,45 @@
+import { ShaderLocalScope } from '../../../../../../api/shaders/source/scope/shader-local-scope';
 import { WglShaderLocalScope } from '../wgl-shader-local-scope';
+import { ShaderExpression } from '../../../../../../api/shaders/source/expression/shader-expression';
+import { ShaderFor } from '../../../../../../api/shaders/source/scope/local-scopes/shader-for';
+import { ShaderWhile } from '../../../../../../api/shaders/source/scope/local-scopes/shader-while';
 
-export class WglShaderEmptyLocalScope extends WglShaderLocalScope {
+export class WglShaderEmptyLocalScope implements ShaderLocalScope {
+
+    public readonly parent: ShaderLocalScope;
+    public readonly child: ShaderLocalScope;
 
     public get scopeName(): string {
         return 'empty-scope';
     }
 
-    constructor(parent: WglShaderEmptyLocalScope) {
-        super(parent);
+    constructor(parent: ShaderLocalScope) {
+        this.parent = parent;
+        this.child = this;
+    }
+
+    public end(): void {
+        throw new Error(`Cannot end the empty scope`);
+    }
+
+    public if(condExpr: ShaderExpression): ShaderLocalScope {
+        throw this.errorNoChildAllowed();
+    }
+
+    public for(initExpr: ShaderExpression, condExpr: ShaderExpression, loopExpr: ShaderExpression): ShaderFor {
+        throw this.errorNoChildAllowed();
+    }
+
+    public while(): ShaderWhile {
+        throw this.errorNoChildAllowed();
     }
 
     public parse(): string {
         return '';
+    }
+
+    private errorNoChildAllowed(): Error {
+        throw new Error(`Cannot add children to the empty scope`);
     }
 
 }
--- src/app/implementations/webgl/shaders/source/scope/wgl-shader-local-scope.ts
+++ src/app/implementations/webgl/shaders/source/scope/wgl-shader-local-scope.ts
@@ -1,24 +1,27 @@
 import { ShaderLocalScope } from '../../../../../api/shaders/source/scope/shader-local-scope';
 import { WglError } from '../../../util/wgl-error';
+import { WglShaderEmptyLocalScope } from './local-scopes/wgl-shader-empty-local-scope';
 
 export abstract class WglShaderLocalScope implements ShaderLocalScope {
 
     public readonly parent: WglShaderLocalScope;
-    public get child(): WglShaderLocalScope {
+    public get child(): ShaderLocalScope {
         return this.childInternal;
     }
 
-    private childInternal: WglShaderLocalScope;
+    private childInternal: ShaderLocalScope;
+    private hasChild = false;
     private hasEnded = false;
 
     public abstract get scopeName(): string;
 
     constructor(parent: WglShaderLocalScope) {
         this.parent = parent;
-        this.childInternal = null;
+        this.childInternal = new WglShaderEmptyLocalScope(this);
         if (parent != null) {
-            if (parent.childInternal == null) {
+            if (!parent.hasChild) {
                 parent.childInternal = this;
+                parent.hasChild = true;
             }
             else {
                 throw new Error(`Cannot set parent of local scope: Parent "${parent.scopeName}" already has a child`);
