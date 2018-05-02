28f6d87f0f66f610ab3ccc0fb82366778a17c799
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-02 17:40:02 -0400

    Made the for class implement the for API interface.

--- src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-for.ts
+++ src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-for.ts
@@ -1,12 +1,20 @@
 import { WglShaderLocalScope } from '../wgl-shader-local-scope';
+import { ShaderFor } from '../../../../../../api/shaders/source/scope/local-scopes/shader-for';
+import { ShaderExpression } from '../../../../../../api/shaders/source/expression/shader-expression';
 
-export class WglShaderFor extends WglShaderLocalScope {
+const TOKEN = 'for';
+
+export class WglShaderFor extends WglShaderLocalScope implements ShaderFor {
+
+    public readonly init: ShaderExpression;
+    public readonly condition: ShaderExpression;
+    public readonly loop: ShaderExpression;
 
     public get scopeName(): string {
         return 'for';
     }
 
-    constructor() {
+    constructor(init: ShaderExpression, condition: ShaderExpression, loop: ShaderExpression) {
         super();
     }
 
