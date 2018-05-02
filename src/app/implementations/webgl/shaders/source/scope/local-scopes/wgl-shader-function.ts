4731385664d88dcf48d9ab22ccbf70df83cb59b6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-02 14:29:48 -0400

    Added a block child inside the function class.

--- src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-function.ts
+++ src/app/implementations/webgl/shaders/source/scope/local-scopes/wgl-shader-function.ts
@@ -6,6 +6,7 @@ import { WglShaderVariable } from '../../expression/lvalues/wgl-shader-variable'
 import { ShaderFunction } from '../../../../../../api/shaders/source/scope/local-scopes/shader-function';
 import { ShaderExpressionType } from '../../../../../../api/shaders/source/expression/shader-expression-type';
 import { WglShaderArgumentListParser } from '../util/wgl-shader-argument-list-parser';
+import { WglShaderBlock } from '../../statement/wgl-shader-block';
 
 export class WglShaderFunction extends WglShaderLocalScope implements ShaderFunction {
 
@@ -30,12 +31,12 @@ export class WglShaderFunction extends WglShaderLocalScope implements ShaderFunc
         this.name = name;
         this.params = params;
         this.ret = ret;
+        this.makeParentOf(new WglShaderBlock([]));
     }
 
     public parse(): string {
-        return this.ret.parse() + ' ' + this.name + new WglShaderArgumentListParser().parseDeclaration(this.params) + ' {\n' +
-            /* TODO */
-            '}';
+        return this.ret.parse() + ' ' + this.name + new WglShaderArgumentListParser().parseDeclaration(this.params) + ' '
+            + this.child.parse();
     }
 
 }
