2f8b70b7c29313aeae4c5aa1b833c4ec9fca054c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-07 22:29:18 -0400

    Made the unit tests of the shader program pass.

--- src/app/implementations/webgl/shaders/wgl-shader-program.ts
+++ src/app/implementations/webgl/shaders/wgl-shader-program.ts
@@ -7,7 +7,14 @@ export class WglShaderProgram implements ShaderProgram {
     public readonly vertexShader: VertexShader;
     public readonly fragmentShader: FragmentShader;
 
+    protected vertexWebGlSource: string;
+    protected fragmentWebGlSource: string;
+
     constructor(vertexShader: VertexShader, fragmentShader: FragmentShader) {
+        this.vertexShader = vertexShader;
+        this.fragmentShader = fragmentShader;
+        this.vertexWebGlSource = this.vertexShader.parse();
+        this.fragmentWebGlSource = this.fragmentShader.parse();
     }
 
 }
