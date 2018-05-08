f35dbc326dfa4fa040cf7df12a8306f2c796e11f
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-07 22:27:14 -0400

    Created testing fragment shader that does not parse.

---
+++ src/app/implementations/webgl/shaders/testing/shaders/wgl-testing-fragment-shader-no-parse.ts
@@ -0,0 +1,6 @@
+import { FragmentShader } from '../../../../../api/shaders/fragment-shader';
+import { WglTestingShaderNoParse } from './wgl-testing-shader-no-parse';
+
+export class WglTestingFragmentShaderNoParse extends WglTestingShaderNoParse implements FragmentShader {
+
+}
