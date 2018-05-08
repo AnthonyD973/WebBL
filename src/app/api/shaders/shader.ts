d49994c3b3d32c4a74843c693cf0cd39ce6ce6ba
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-07 21:33:07 -0400

    Added a global scope in the API shader interface.

--- src/app/api/shaders/shader.ts
+++ src/app/api/shaders/shader.ts
@@ -1,5 +1,8 @@
 import { ShaderParsable } from './source/shader-parsable';
+import { ShaderGlobalScope } from './source/scope/shader-global-scope';
 
 export interface Shader extends ShaderParsable {
+
+    readonly globalScope: ShaderGlobalScope;
 
 }
