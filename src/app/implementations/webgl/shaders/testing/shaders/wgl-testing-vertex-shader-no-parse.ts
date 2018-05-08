0ea38c9db2af6093b8cb9ad85a84788748e8de1a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-07 22:21:45 -0400

    Created testing vertex shader that does not parse.

---
+++ src/app/implementations/webgl/shaders/testing/shaders/wgl-testing-vertex-shader-no-parse.ts
@@ -0,0 +1,6 @@
+import { VertexShader } from '../../../../../api/shaders/vertex-shader';
+import { WglTestingShaderNoParse } from './wgl-testing-shader-no-parse';
+
+export class WglTestingVertexShaderNoParse extends WglTestingShaderNoParse implements VertexShader {
+
+}
