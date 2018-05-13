1346947d4c07ae8a4a4b0addac9967cdf4cf482a
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-13 19:30:01 -0400

    Partially implemented the end method of the program class.

--- src/app/implementations/webgl/shaders/wgl-shader-program.ts
+++ src/app/implementations/webgl/shaders/wgl-shader-program.ts
@@ -1,6 +1,8 @@
 import { ShaderProgram } from '../../../api/shaders/shader-program';
 import { FragmentShader } from '../../../api/shaders/fragment-shader';
 import { VertexShader } from '../../../api/shaders/vertex-shader';
+import { WglVertexShader } from './wgl-vertex-shader';
+import { WglFragmentShader } from './wgl-fragment-shader';
 
 export class WglShaderProgram implements ShaderProgram {
 
@@ -10,14 +12,35 @@ export class WglShaderProgram implements ShaderProgram {
     protected gl: WebGLRenderingContext;
     protected glProgram: WebGLProgram;
 
-    constructor(gl: WebGLRenderingContext, vertexShader: VertexShader, fragmentShader: FragmentShader) {
+    protected hasEnded = false;
+
+    constructor(gl: WebGLRenderingContext, vertexShader: WglVertexShader, fragmentShader: WglFragmentShader) {
         this.gl = gl;
+        this.glProgram = this.gl.createProgram();
+
         this.vertexShader = vertexShader;
         this.fragmentShader = fragmentShader;
     }
 
     public end(): void {
+        this.assertHasNotEnded();
 
+        this.vertexShader.compile();
+        this.fragmentShader.compile();
+
+        this.gl.linkProgram(this.glProgram);
+        if (this.gl.getProgramParameter(this.glProgram, this.gl.LINK_STATUS)) {
+            this.hasEnded = true;
+        }
+        else {
+            throw new Error(`Could not link WebGL program: ${this.gl.getProgramInfoLog(this.glProgram)}`);
+        }
+    }
+
+    protected assertHasNotEnded(): void {
+        if (this.hasEnded) {
+            throw new Error(`Cannot perform this operation on shader program: program is already compiled, linked and locked`);
+        }
     }
 
 }
