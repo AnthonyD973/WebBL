36aee4a9cb13b7aa967a4220bc469335713c5a9c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-13 17:25:46 -0400

    Implemented specs for the WglShader.

---
+++ src/app/implementations/webgl/shaders/wgl-shader.spec.ts
@@ -0,0 +1,50 @@
+import { WglShader } from './wgl-shader';
+
+class TestingShaderValid extends WglShader {
+    constructor(gl: WebGLRenderingContext) {
+        super(gl, gl.VERTEX_SHADER);
+    }
+    public parse(): string {
+        return 'void main() { gl_Position = vec4(0.0, 0.0, 0.0, 0.0); }';
+    }
+}
+
+class TestingShaderNoCompile extends WglShader {
+    constructor(gl: WebGLRenderingContext) {
+        super(gl, gl.VERTEX_SHADER);
+    }
+    public parse(): string {
+        return 'THIS STRING IS NOT A VALID GLSL VERTEX SHADER, UNLESS CHUCK N.RRIS DECIDES OTHERWISE';
+    }
+}
+
+describe(WglShader.name, () => {
+
+    let gl: WebGLRenderingContext;
+    let validShader: TestingShaderValid;
+    let invalidShader: TestingShaderNoCompile;
+
+    beforeEach(() => {
+        gl = document.createElement('canvas').getContext('webgl');
+        validShader = new TestingShaderValid(gl);
+        invalidShader = new TestingShaderNoCompile(gl);
+    });
+
+    it('should be created', () => {
+        expect(validShader).toBeTruthy();
+        expect(validShader.globalScope).toBeTruthy();
+    });
+
+    describe('compile', () => {
+
+        it('should not throw an error with a valid shader', () => {
+            validShader.compile();
+        });
+
+        it('should throw an error with a shader that parses but does not compile', () => {
+            expect(() => invalidShader.compile()).toThrow();
+        });
+
+    });
+
+});
