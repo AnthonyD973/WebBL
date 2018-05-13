1975000be66280a155f08837bd55eff709120082
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-13 17:26:59 -0400

    Implemented the WglShader so that the tests pass.

--- src/app/implementations/webgl/shaders/wgl-shader.ts
+++ src/app/implementations/webgl/shaders/wgl-shader.ts
@@ -11,10 +11,15 @@ export abstract class WglShader implements Shader {
     constructor(gl: WebGLRenderingContext, shaderType: number) {
         this.gl = gl;
         this.glShader = this.gl.createShader(shaderType);
+        this.globalScope = new WglShaderGlobalScope();
     }
 
     public compile(): void {
-
+        this.gl.shaderSource(this.glShader, this.parse());
+        this.gl.compileShader(this.glShader);
+        if (!this.gl.getShaderParameter(this.glShader, this.gl.COMPILE_STATUS)) {
+            throw new Error(`WebGL shader could not compile: ${this.gl.getShaderInfoLog(this.glShader)}`);
+        }
     }
 
     public abstract parse(): string;
