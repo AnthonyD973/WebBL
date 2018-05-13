aa713e728b499b13113021c9960ab90d0ee034db
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-13 15:33:26 -0400

    Added methods to create programs and shaders on the webbl rendering context.

--- src/app/api/rendering-context/webbl-rendering-context.ts
+++ src/app/api/rendering-context/webbl-rendering-context.ts
@@ -1,5 +1,13 @@
+import { VertexShader } from '../shaders/vertex-shader';
+import { FragmentShader } from '../shaders/fragment-shader';
+import { ShaderProgram } from '../shaders/shader-program';
+
 export interface WebBLRenderingContext {
 
     readonly canvas: HTMLCanvasElement;
+
+    createShaderProgram(vs: VertexShader, fs: FragmentShader): ShaderProgram;
+    createVertexShader(): VertexShader;
+    createFragmentShader(): FragmentShader;
 
 }
--- src/app/implementations/webgl/rendering-context.ts/webbl-rendering-context-for-webgl.ts
+++ src/app/implementations/webgl/rendering-context.ts/webbl-rendering-context-for-webgl.ts
@@ -1,4 +1,7 @@
 import { WebBLRenderingContext } from '../../../api/rendering-context/webbl-rendering-context';
+import { VertexShader } from '../../../api/shaders/vertex-shader';
+import { FragmentShader } from '../../../api/shaders/fragment-shader';
+import { ShaderProgram } from '../../../api/shaders/shader-program';
 
 export class WebBLRenderingContextForWebGL implements WebBLRenderingContext {
 
@@ -15,6 +18,18 @@ export class WebBLRenderingContextForWebGL implements WebBLRenderingContext {
         else {
             throw new Error(`Cannot create WebBL rendering context on a null canvas`);
         }
+    }
+
+    public createShaderProgram(vs: VertexShader, fs: FragmentShader): ShaderProgram {
+        return null;
+    }
+
+    public createVertexShader(): VertexShader {
+        return null;
+    }
+
+    public createFragmentShader(): FragmentShader {
+        return null;
     }
 
 }
