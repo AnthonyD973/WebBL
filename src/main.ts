fb52b3a1d448e05a005af3726a45d3f0bfa02266
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-13 21:13:43 -0400

    Made the main call the WebBL setup function.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { setUpWebBl } from './app/web-bl-main';
 
 if (environment.production) {
   enableProdMode();
 }
+
+setUpWebBl();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
