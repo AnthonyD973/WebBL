4c2bdf7f7087f2f823a15329adc6bfa5c9352f09
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-13 21:03:41 -0400

    Added return of the WebBL context in the getContext function.

--- src/app/web-bl-main.ts
+++ src/app/web-bl-main.ts
@@ -1,17 +1,30 @@
+import { WebBLRenderingContextForWebGL } from './implementations/webgl/rendering-context.ts/webbl-rendering-context-for-webgl';
+
 let setUpAlreadyDone = false;
+const WEBBL_PREFIX = 'webbl-';
 
 export function setUpWebBl() {
     if (!setUpAlreadyDone) {
         const canvasPrototype = HTMLCanvasElement.prototype;
         const getContext = canvasPrototype.getContext;
+
         const webBlGetContext = function(str) {
-            if (str === 'webbl') {
-                return null; // TODO
+            const webBLRegex = new RegExp(`^${WEBBL_PREFIX}.*$`);
+            if (webBLRegex.test(str)) {
+                // Use WebBL rendering context
+                const implementation = str.match(new RegExp(`(?<=${WEBBL_PREFIX}).*`))[0];
+                if (implementation === 'webgl') {
+                    return new WebBLRenderingContextForWebGL(this);
+                }
+                else {
+                    throw new Error(`Requested WebBL implementation "${implementation}" does not exist`);
+                }
             }
             else {
                 return getContext.bind(this)(str);
             }
         };
+
         canvasPrototype.getContext = webBlGetContext;
         setUpAlreadyDone = true;
     }
