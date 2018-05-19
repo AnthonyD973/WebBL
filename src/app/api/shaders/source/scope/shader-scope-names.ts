058277fb5295383d7578dd07fb0fafa443bf4b3b
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-19 17:30:24 -0400

    Created class ShaderScopeNames.

---
+++ src/app/api/shaders/source/scope/shader-scope-names.ts
@@ -0,0 +1,11 @@
+export abstract class ShaderScopeNames {
+
+    public static readonly emptyScope = '<empty scope>';
+    public static readonly if         = 'if';
+    public static readonly elseIf     = 'else if';
+    public static readonly else       = 'else';
+    public static readonly for        = 'for';
+    public static readonly while      = 'while';
+    public static readonly function   = 'function';
+
+}
