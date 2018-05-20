dfbd22621ef69b519fbbff8c41102b733c11a22c
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-19 22:14:59 -0400

    Added method addChild to the testing shader.

--- src/app/implementations/webgl/shaders/testing/shaders/wgl-testing-shader.ts
+++ src/app/implementations/webgl/shaders/testing/shaders/wgl-testing-shader.ts
@@ -1,6 +1,7 @@
 import { Shader } from '../../../../../api/shaders/shader';
 import { ShaderGlobalScope } from '../../../../../api/shaders/source/scope/shader-global-scope';
 import { WglShaderGlobalScope } from '../../source/scope/wgl-shader-global-scope';
+import { ShaderScope } from '../../../../../api/shaders/source/scope/shader-scope';
 
 export class WglTestingShader implements Shader {
 
@@ -16,6 +17,10 @@ export class WglTestingShader implements Shader {
 
     public compile(): void {
         this.parse();
+    }
+
+    public addChild(c: ShaderScope): void {
+        throw new Error(`Cannot add children to a shader, not even the test shader`);
     }
 
 }
