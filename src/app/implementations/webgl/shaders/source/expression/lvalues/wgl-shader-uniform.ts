f11af0fe723fb887e73a988dc6da9750e0655369
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-20 13:53:51 -0400

    Made the tests pass.

--- src/app/implementations/webgl/shaders/source/expression/lvalues/wgl-shader-uniform.ts
+++ src/app/implementations/webgl/shaders/source/expression/lvalues/wgl-shader-uniform.ts
@@ -1,6 +1,12 @@
 import { WglShaderInput } from './wgl-shader-input';
 import { ShaderUniform } from '../../../../../../api/shaders/source/expression/lvalues/shader-uniform';
 
+const TOKEN = 'uniform';
+
 export class WglShaderUniform extends WglShaderInput implements ShaderUniform {
+
+    public parse(): string {
+        return TOKEN + ' ' + super.parse();
+    }
 
 }
