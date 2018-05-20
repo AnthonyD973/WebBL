5302a7364052f0147c0d70ac05dcdcb9ad3fa0a0
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-19 22:19:08 -0400

    Made the Shader API interface extends the ShaderScope API interface.

--- src/app/api/shaders/shader.ts
+++ src/app/api/shaders/shader.ts
@@ -1,7 +1,8 @@
 import { ShaderParsable } from './source/shader-parsable';
 import { ShaderGlobalScope } from './source/scope/shader-global-scope';
+import { ShaderScope } from './source/scope/shader-scope';
 
-export interface Shader extends ShaderParsable {
+export interface Shader extends ShaderParsable, ShaderScope {
 
     readonly globalScope: ShaderGlobalScope;
 
