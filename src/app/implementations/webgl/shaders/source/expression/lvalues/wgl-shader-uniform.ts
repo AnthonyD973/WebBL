8ae39313a7766e88ea22f1569d244231a966fb6e
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-20 13:44:18 -0400

    Created WebGL class for uniforms.

---
+++ src/app/implementations/webgl/shaders/source/expression/lvalues/wgl-shader-uniform.ts
@@ -0,0 +1,6 @@
+import { WglShaderInput } from './wgl-shader-input';
+import { ShaderUniform } from '../../../../../../api/shaders/source/expression/lvalues/shader-uniform';
+
+export class WglShaderUniform extends WglShaderInput implements ShaderUniform {
+
+}
