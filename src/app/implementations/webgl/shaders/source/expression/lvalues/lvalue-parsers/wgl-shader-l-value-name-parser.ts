6d511d8b9c847f5be674925f447c25b5f672afe6
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-05-21 14:15:07 -0400

    Created class for the lvalue name parsing.

---
+++ src/app/implementations/webgl/shaders/source/expression/lvalues/lvalue-parsers/wgl-shader-l-value-name-parser.ts
@@ -0,0 +1,18 @@
+// tslint:disable-next-line:max-line-length
+import { ShaderLValueNameParser } from '../../../../../../../api/shaders/source/expression/lvalues/lvalue-parsers/shader-l-value-name-parser';
+import { ShaderExpressionType } from '../../../../../../../api/shaders/source/expression/shader-expression-type';
+import { ShaderVariable } from '../../../../../../../api/shaders/source/expression/lvalues/shader-variable';
+
+export class WglShaderLValueNameParser implements ShaderLValueNameParser {
+
+    public readonly type: ShaderExpressionType;
+    public readonly variable: ShaderVariable;
+
+    constructor(variable: ShaderVariable) {
+    }
+
+    public parse(): string {
+        return null;
+    }
+
+}
