926e89439d8bbdb81ae835f57942cca440eceb78
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-06-17 17:37:34 -0400

    Created GettingStarted component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BlackCanvasComponent } from './implementations/webgl/examples/black-canvas/black-canvas.component';
+import { GettingStartedComponent } from './implementations/webgl/examples/mdn-tutorial/getting-started/getting-started.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        BlackCanvasComponent
+        BlackCanvasComponent,
+        GettingStartedComponent
     ],
     imports: [
         BrowserModule
