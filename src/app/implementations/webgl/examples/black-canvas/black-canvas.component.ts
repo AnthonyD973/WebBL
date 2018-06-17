95fb3cc942f40a3dd5def66ef294765ee8609779
Anthony Dentinger <anthony.dentinger@polymtl.ca>
2018-06-17 17:15:14 -0400

    example step 9: Removed loadShader.

--- src/app/implementations/webgl/examples/black-canvas/black-canvas.component.ts
+++ src/app/implementations/webgl/examples/black-canvas/black-canvas.component.ts
@@ -228,33 +228,6 @@ export class BlackCanvasComponent implements OnInit {
             }
         }
 
-        //
-        // creates a shader of the given type, uploads the source and
-        // compiles it.
-        //
-        function loadShader(gl, type, source) {
-            const shader = gl.createShader(type);
-
-            // Send the source to the shader object
-
-            gl.shaderSource(shader, source);
-
-            // Compile the shader program
-
-            gl.compileShader(shader);
-
-            // See if it compiled successfully
-
-            if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-                alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-                gl.deleteShader(shader);
-                return null;
-            }
-
-            return shader;
-        }
-
-
         main(bL);
 
         // const vs = bl.createVertexShader();
